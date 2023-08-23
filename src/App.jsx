524e3a761b993393f1c1cdbc893eaf7f36e4953b
rogerworkup <rogerworkup@gmail.com>
2023-08-23 20:55:36 -0300

    Antes de por a foto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import MainContent from "./componentes/MainContent";
+import Sidebar from "./componentes/Sidebar";
+
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <div id="portfolio">
       <h1> Rogério Andrade</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   );
 }

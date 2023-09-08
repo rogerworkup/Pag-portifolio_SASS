952d48a8d462d849350f38ea59ae168b6c1afc20
rogerworkup <rogerworkup@gmail.com>
2023-09-08 18:15:55 -0300

    Adicionado foto, configurado sidebar com estilizacao do botao

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,8 +1,19 @@
+import Avatar from "../img/eu.jpg"
 
+import '../styles/components/sidebar.sass'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="Rogerio Andrade" />
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">
+        Download Currículo
+        </a>
+
+    </aside>
   )
 }
 

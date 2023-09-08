7bc348766f4ca26c2c90e46e06130b2357ded122
rogerworkup <rogerworkup@gmail.com>
2023-09-08 20:07:44 -0300

    Configurado botoes de socialnetworks, identificado problema na responsividade da imagem de perfil

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,13 +1,16 @@
+import SocialNetworks from "./SocialNetworks"
+
 import Avatar from "../img/eu.jpg"
 
 import '../styles/components/sidebar.sass'
+
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Rogerio Andrade" />
-      <p className="title">Desenvolvedor</p>
-      <p>redes sociais</p>
+      <p className="title">Full Stack Developer</p>
+      <SocialNetworks />
       <p>informações de contato</p>
       <a href="" className="btn">
         Download Currículo

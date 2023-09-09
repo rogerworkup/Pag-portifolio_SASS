ac4be3aaef5a8d2e5cc48c0de39e254afdcd2c9a
rogerworkup <rogerworkup@gmail.com>
2023-09-08 21:38:20 -0300

    Estilizacao do maincontent concluida

---
+++ src/componentes/AboutContainer.jsx
@@ -0,0 +1,14 @@
+
+
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+        <h2>Sobre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nam explicabo delectus necessitatibus, dignissimos quo eveniet assumenda dicta modi laboriosam libero voluptate similique cupiditate consequatur sint, cum quia, accusantium iusto?</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae ullam modi sunt ex perferendis repudiandae repellat unde. Necessitatibus iusto fuga similique enim dolor dolores natus debitis error blanditiis accusantium?</p>
+    </section>
+  )
+}
+
+export default AboutContainer

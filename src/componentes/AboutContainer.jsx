a4121f3805779ef85d222d13411f478372ebb954
rogerworkup <rogerworkup@gmail.com>
2023-09-08 23:07:39 -0300

    Finalizado conforme tutorial

--- src/componentes/AboutContainer.jsx
+++ src/componentes/AboutContainer.jsx
@@ -4,9 +4,9 @@ const AboutContainer = () => {
   return (
     <section className="about-container">
         <h2>Sobre</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nam explicabo delectus necessitatibus, dignissimos quo eveniet assumenda dicta modi laboriosam libero voluptate similique cupiditate consequatur sint, cum quia, accusantium iusto?</p>
+        <p>Sargento do Exército Brasileiro desde 2017, tecnólogo em Análise e desenvolvimento de sistemas pós graduado em Segurança da informação e em Análise de dados. Responsável técnico pela seção de TI de um quartel, com experiência em exercer atividades como: Administração de redes e servidores, segurança da informação, montagem e manutenção de computadores, apoio ao usuário e desenvolvimento de sistemas.</p>
         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae ullam modi sunt ex perferendis repudiandae repellat unde. Necessitatibus iusto fuga similique enim dolor dolores natus debitis error blanditiis accusantium?</p>
+        <p>Entusiasta da tecnologia desde sempre, adaptado a liderar e/ou ser liderado lidando com variadas situações e com diferentes pessoas, competência adquirida em anos compondo as fileiras do Exército. Atualmente focado em aprender e adquirir experiências na área de desenvolvimento e mais do que isso, utilizar a tecnologia e as metodologias do ensino para impactar em larga escala a educação na sociedade.</p>
     </section>
   )
 }
--- src/componentes/ProjectsContainer.jsx
+++ src/componentes/ProjectsContainer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ProjectsContainer = () => {
   return (
-    <div>ProjectsContainer</div>
+    <section className="projects-container">
+        <h2>Projetos</h2>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem enim sed dignissimos quod ea, laboriosam similique placeat fugiat nisi nostrum cumque veritatis, possimus quasi, tenetur dolorem accusamus voluptas vel maxime!
+        </p>
+        <a href="#" className="btn">
+            Ver Projetos
+        </a>
+    </section>
   )
 }
 

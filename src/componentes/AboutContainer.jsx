1335a73b19483291fd213fd7bf14a734c93ee0b3
rogerworkup <rogerworkup@gmail.com>
2023-10-01 15:52:48 -0300

    Lista no about formatada

--- src/componentes/AboutContainer.jsx
+++ src/componentes/AboutContainer.jsx
@@ -6,8 +6,9 @@ const AboutContainer = () => {
   return (
     <section className="about-container">        
           <h2>Sobre</h2>
-        <p>Sargento do Exército Brasileiro desde 2017. Responsável técnico pela seção de TI de um quartel, com experiência em exercer atividades como:
+        <p>Sargento do Exército Brasileiro desde 2017. <b>Analista de TI</b>, responsável técnico pela seção de TI de um quartel, com experiência em exercer atividades como:
 
+        <div className="list">
         <ul>
           <li>Administração de redes e servidores;</li>
           <li>Segurança da informação;</li>
@@ -15,10 +16,11 @@ const AboutContainer = () => {
           <li>Apoio ao usuário;</li>
           <li>Desenvolvimento de sistemas.</li>
         </ul>
+        </div>
 
         </p>
         
-        <p>Atualmente focado em aprender e adquirir experiências na área de desenvolvimento e mais do que isso, utilizar a tecnologia e as metodologias do ensino para impactar em larga escala a educação na sociedade.</p>
+        <p>Atualmente focado em aprender e adquirir experiências na área de desenvolvimento e mais do que isso, utilizar a tecnologia e as metodologias do ensino para <b>impactar em larga escala</b> a educação na sociedade.</p>
     </section>
   )
 }

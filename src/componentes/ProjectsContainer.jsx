47764465c9a3fcb72afc5381bab9095db5668fa6
rogerworkup <rogerworkup@gmail.com>
2023-10-02 13:07:48 -0300

    links no socialnetworks adicionados

--- src/componentes/ProjectsContainer.jsx
+++ src/componentes/ProjectsContainer.jsx
@@ -1,35 +1,33 @@
-import mns_project from "../img/MyNeighborhoodSpeak-cropped.png"
+import mns_project from "../img/MyNeighborhoodSpeak.png";
 
 import "../styles/components/projectscontainer.sass";
 
 const projects = [
   {
-  name: "My Neighborhood Speak",
-  icon: <img src={mns_project} alt="MnS" />,
-  description: "Quiz sobre a língua inglesa com premiação em Pix (R$) para os primeiros colocados. Desafio que incentiva alunos do ensino médio das escolas do bairro a praticar o idioma, este que é tão importante para o mercado de trabalho e para a vida.",
+    name: "My Neighborhood Speak",
+    icon: <img src={mns_project} alt="MnS" />,
+    description:
+      "Quiz sobre a língua inglesa com premiação em Pix (R$) para os primeiros colocados. Desafio que incentiva alunos do ensino médio das escolas do bairro a praticar o idioma, este que é tão importante para o mercado de trabalho e para a vida.",
   },
 ];
 
 const ProjectsContainer = () => {
   return (
     <section className="projects-container">
-        <h2>Projetos</h2>
-        <div className="projects-grid">
+      <h2>Projetos</h2>
+      <div className="projects-grid">
         {projects.map((proj) => (
           <div className="projects-card" id={proj.id} key={proj.id}>
             {proj.icon}
             <div className="projects-info">
               <h3>{proj.name}</h3>
-              <p>
-                {proj.description}
-              </p>
+              <p>{proj.description}</p>
             </div>
-            
           </div>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectsContainer
+export default ProjectsContainer;
--- src/componentes/SocialNetworks.jsx
+++ src/componentes/SocialNetworks.jsx
@@ -1,25 +1,51 @@
-import {FaLinkedinIn, FaGithub, FaWhatsapp, FaInstagram} from 'react-icons/fa'
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaWhatsapp,
+  FaInstagram,
+} from "react-icons/fa";
 
-
-import "../styles/components/socialnetworks.sass"
+import "../styles/components/socialnetworks.sass";
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn /> },
-  { name: "github", icon: <FaGithub /> },
-  { name: "whatsapp", icon: <FaWhatsapp /> },
-  { name: "instagram", icon: <FaInstagram /> },
+  {
+    name: "linkedin",
+    way: "https://www.linkedin.com/in/rog%C3%A9rio-andrade-38a755111/",
+    icon: <FaLinkedinIn />,
+  },
+  {
+    name: "github",
+    way: "https://github.com/rogerworkup",
+    icon: <FaGithub />,
+  },
+  {
+    name: "whatsapp",
+    way: "https://wa.me/5581986684427?text=Olá,%20vim%20através%20da%20sua%20página%20de%20portifólio.",
+    icon: <FaWhatsapp />,
+  },
+  {
+    name: "instagram",
+    way: "https://instagram.com/rogeralado",
+    icon: <FaInstagram />,
+  },
 ];
 
 const SocialNetworks = () => {
   return (
     <section id="social-networks">
-      {socialNetworks.map((network) =>(
-        <a href="#" className='social-btn' id={network.name} key={network.name}>
+      {socialNetworks.map((network) => (
+        <a
+          href={network.way}
+          className="social-btn"
+          id={network.name}
+          key={network.name}
+          target="_blank"
+        >
           {network.icon}
         </a>
       ))}
     </section>
-  )
-}
+  );
+};
 
-export default SocialNetworks
+export default SocialNetworks;
